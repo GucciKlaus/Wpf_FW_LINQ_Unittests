57888f4abf58a7a4703452a3cbf09f74b168fdba
Klaus Hoehfurtner <HoehfurtnerK190028@sus.htl-grieskirchen.at>
2024-01-08 14:37:03 +0100

    unit-tests

---
+++ UnitTestProjectLINQ/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectLINQ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OEM Company")]
+[assembly: AssemblyProduct("UnitTestProjectLINQ")]
+[assembly: AssemblyCopyright("Copyright © OEM Company 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e511ee4b-6654-4c1a-95b4-1e37eff2eb1c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProjectLINQ/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace UnitTestProjectLINQ
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod_AnzUntersch_Buchstaben()
+        {
+            int count = WpfFW_LING_Unittests.MainWindow.AnzahlUnterschiedlicheBuchstaben_Lambda(WpfFW_LING_Unittests.MainWindow.name);
+            Assert.AreEqual(16, count);
+        }
+        [TestMethod]
+        public void TestMethod_AnzNamenmitBuchstabenLambda()
+        {
+            List<string> stringList = new List<string> { "FRANZ", "EMIL", "BERTA", "ANDI" };
+            List<string> listnew = WpfFW_LING_Unittests.MainWindow.NamenK6GRoss_Lambda(WpfFW_LING_Unittests.MainWindow.stringList);
+            CollectionAssert.AreEqual(stringList, listnew);
+        }
+        [TestMethod]
+        public void TestMethod_AnzNamenmitBuchstabenQuery()
+        {
+            List<string> stringList = new List<string> { "FRANZ", "EMIL", "BERTA", "ANDI" };
+            List<string> listnew = WpfFW_LING_Unittests.MainWindow.NamenK6GRoss_Query(WpfFW_LING_Unittests.MainWindow.stringList);
+            CollectionAssert.AreEqual(stringList, listnew);
+        }
+    }
+}
